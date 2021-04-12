aa3b892dca7c5825427d5e86df731218a1539914
PogrebenkoBS81 <pogrebenko.vasily@gmail.com>
2021-04-12 23:13:08 +0300

    Comments added to the DBType enum

--- lab3/src/pogrebenko/lab3db/controller/DBType.java
+++ lab3/src/pogrebenko/lab3db/controller/DBType.java
@@ -1,5 +1,12 @@
 package pogrebenko.lab3db.controller;
 
+/**
+ * Supported DBs for the medicines.
+ *
+ * @author Pogrebenko Vasily, BS-81
+ * @version 1.3.0
+ * @since 1.3.0
+ */
 @SuppressWarnings("SameParameterValue")
 public enum DBType {
     MYSQL("MySQL");
@@ -8,10 +15,20 @@ public enum DBType {
 
     private final String label;
 
+    /**
+     * Constructs DBType.
+     *
+     * @param label label of the DB.
+     */
     DBType(String label) {
         this.label = label;
     }
 
+    /**
+     * Returns the DB label.
+     *
+     * @return string representation of the DBType.
+     */
     public String toString() {
         return label;
     }
